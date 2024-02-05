0e95732f496f38f33b9b19015fbb8793a5ed5bc6
kevin <zeradhenny474@gmail.com>
2024-02-05 15:39:28 -0500

    complete home page

---
+++ assets/custom-arrow.js
@@ -0,0 +1,9 @@
+document.querySelector(".slider-button--prev").addEventListener('click', function(event){
+    console.log("lckckdklakdf");
+    document.querySelector(".flickity-prev-next-button.previous").click();
+
+});
+document.querySelector(".slider-button--next").addEventListener("click", function (event) {
+    console.log("lckckdklakdf");
+    document.querySelector(".flickity-prev-next-button.next").click();
+  });
